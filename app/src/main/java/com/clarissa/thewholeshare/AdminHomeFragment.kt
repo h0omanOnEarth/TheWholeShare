51b66e26f561e5fb335daff8326f8e6087f9c33b
Clarissa Gracienne <clarissagracia.cg@gmail.com>
2023-01-02 15:43:21 +0700

    Id for home admin

--- app/src/main/java/com/clarissa/thewholeshare/AdminHomeFragment.kt
+++ app/src/main/java/com/clarissa/thewholeshare/AdminHomeFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class AdminHomeFragment : Fragment() {
+
+    lateinit var tvTotalLocations : TextView
+    lateinit var tvTotalPackages_OnGoing: TextView
+    lateinit var tvTotalPackages_Finished : TextView
+    lateinit var tvTotalPackages_Canceled : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,14 @@ class AdminHomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admin_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvTotalLocations = view.findViewById(R.id.tvTotalLoc_homeAdmin)
+        tvTotalPackages_OnGoing = view.findViewById(R.id.tvTotalPackagesOnGoing_homeAdmin)
+        tvTotalPackages_Canceled = view.findViewById(R.id.tvTotalPackCanceled_HomeAdmin)
+        tvTotalPackages_Finished = view.findViewById(R.id.tvTotalFinishedPackage_homeAdmin)
     }
 
 
