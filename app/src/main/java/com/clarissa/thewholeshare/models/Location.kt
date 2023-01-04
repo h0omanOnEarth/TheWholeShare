f8214d5bb1ddaf15a87bf0595fd245bcf9aea43c
sachio271 <105487625+sachio271@users.noreply.github.com>
2023-01-04 20:17:32 +0700

    add location

--- app/src/main/java/com/clarissa/thewholeshare/models/Location.kt
+++ app/src/main/java/com/clarissa/thewholeshare/models/Location.kt
@@ -3,8 +3,10 @@ package com.clarissa.thewholeshare.models
 data class Location(
     var id:Int,
     var address : String,
+    var batch : Int,
+    var deadline : String,
     var note : String,
-    var status : Int,
+    var status : String,
     var deleted_at : String?=null
 ) {
 }
