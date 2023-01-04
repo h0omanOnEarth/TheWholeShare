29dd5c01fb939de6cbbe99c7402f417b4aedb953
DraSelXL <uni.nathanwill02@gmail.com>
2023-01-04 09:42:10 +0700

    Clean the codebase by removing unused codes

--- app/src/main/java/com/clarissa/thewholeshare/LoginActivity.kt
+++ app/src/main/java/com/clarissa/thewholeshare/LoginActivity.kt
@@ -31,8 +31,31 @@ class LoginActivity : AppCompatActivity() {
     lateinit var etUsername : EditText
     lateinit var etPassword : EditText
 
-    //mutable list untuk memasukkan daftar users
-    lateinit var arrUsers : MutableList<User>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnBack = findViewById(R.id.btnBack_Login)
+        etUsername = findViewById(R.id.etUsername_Login)
+        etPassword = findViewById(R.id.etPassword_Login)
+
+        // When the login button is pressed,
+        btnLogin.setOnClickListener {
+            // Validate that the input fields are not empty
+            if (etUsername.text.toString() == "" || etPassword.toString() == "") {
+                alertDialogFailed("ERROR","Fill all the fields!")
+            }
+
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            attemptLogin(username, password)
+        }
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
 
     /**
      * Attempt to log in to the application using the provided username and password.
@@ -91,136 +114,12 @@ class LoginActivity : AppCompatActivity() {
                 alertDialogFailed("Login Failed!", error.toString())
             }
         )
+        // Setting the retry connection policy before concluding a timeout event for the request
         loginRequest.retryPolicy =
             DefaultRetryPolicy(5000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
 
+        // Add the request object to the service queue
         WholeShareApiService.getInstance(this).addToRequestQueue(loginRequest)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        btnLogin = findViewById(R.id.btnLogin)
-        btnBack = findViewById(R.id.btnBack_Login)
-        etUsername = findViewById(R.id.etUsername_Login)
-        etPassword = findViewById(R.id.etPassword_Login)
-        arrUsers = mutableListOf()
-
-        refreshList()
-
-        btnLogin.setOnClickListener {
-            val username = etUsername.text.toString()
-            val password = etPassword.text.toString()
-
-            attemptLogin(username, password)
-
-//            if(username!="" && password!=""){
-//                //maka lakukan login
-//                if(isUsernameRegistered(username)==true) {
-//
-//                    if(isPasswordTrue(username,password)!=-1){
-//                        val role = isPasswordTrue(username,password)
-//
-//                        if(role==1){
-//                            //user
-//                            val intent = Intent(this@LoginActivity, UserMainActivity::class.java)
-//                            intent.putExtra("active_username",username)
-//                            startActivity(intent)
-//                        }else if(role==2){
-//                            //admin
-//                            val intent = Intent(this@LoginActivity, AdminMainActivity::class.java)
-//                            intent.putExtra("active_username",username)
-//                            startActivity(intent)
-//                        }else if(role==3){
-//                            //driver
-//                            val intent = Intent(this@LoginActivity, DriverMainActivity::class.java)
-//                            intent.putExtra("active_username",username)
-//                            startActivity(intent)
-//                        }
-//
-//                        etUsername.text.clear()
-//                        etPassword.text.clear()
-//
-//                    }else{
-//                        alertDialogFailed("ERROR","Wrong password!")
-//                    }
-//
-//                }else{
-//                    alertDialogFailed("ERROR","Username isn't registered!")
-//                }
-//
-//            }else{
-//                alertDialogFailed("ERROR","Fill all the fields!")
-//            }
-        }
-
-        btnBack.setOnClickListener {
-            finish()
-        }
-    }
-
-    fun isPasswordTrue(uname: String, pass:String):Int{
-       var theRole = -1
-
-        for(i in arrUsers.indices){
-            if(arrUsers[i].username==uname){
-                if(arrUsers[i].password==pass){
-                    theRole = arrUsers[i].role
-                    break
-                }
-            }
-        }
-
-        return theRole
-    }
-
-    //fungsi untuk melakukan login
-    fun isUsernameRegistered(uname:String):Boolean{
-        var isAda = false
-        for(i in arrUsers.indices){
-            if(arrUsers[i].username==uname){
-                isAda = true
-                break
-            }
-        }
-        return  isAda
-    }
-
-    //untuk refresh list atau memasukkan data ke dalam mutable list :
-    fun refreshList(){
-        val strReq = object: StringRequest(
-            Method.GET,
-            "${WholeShareApiService.WS_HOST}/listUsers",
-            Response.Listener {
-                val obj: JSONArray = JSONArray(it)
-                arrUsers.clear()
-                println(obj.length())
-                for (i in 0 until obj.length()){
-                    val o = obj.getJSONObject(i)
-                    println(o)
-                    val id = o.getInt("id")
-                    val username = o.getString("username")
-                    val password = o.getString("password")
-                    val full_name = o.getString("full_name")
-                    val phone = o.getString("phone")
-                    val address = o.getString("address")
-                    val email = o.getString("email")
-                    val role = o.getInt("role")
-                    val deleted_at = o.get("deleted_at").toString()
-                    val u = User(
-                        id,username,password,full_name,phone,address,email,role,deleted_at
-                    )
-                    arrUsers.add(u)
-
-                }
-                println(arrUsers.size)
-            },
-            Response.ErrorListener {
-                Toast.makeText(this,"ERROR!", Toast.LENGTH_SHORT).show()
-            }
-        ){}
-        val queue: RequestQueue = Volley.newRequestQueue(this)
-        queue.add(strReq)
     }
 
     //alert dialog warning
