350351672407eede7b4ea6e8d4a009ea42304e93
DraSelXL <uni.nathanwill02@gmail.com>
2023-01-09 08:25:12 +0700

    Connect the courier home fragment to the server

--- app/src/main/java/com/clarissa/thewholeshare/DriverMainActivity.kt
+++ app/src/main/java/com/clarissa/thewholeshare/DriverMainActivity.kt
@@ -2,6 +2,7 @@ package com.clarissa.thewholeshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -10,13 +11,13 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.clarissa.thewholeshare.api.WholeShareApiService
 import com.clarissa.thewholeshare.fragments.*
 import com.clarissa.thewholeshare.models.User
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.json.JSONArray
 
 class DriverMainActivity : AppCompatActivity() {
-
     lateinit var navbar_driver : BottomNavigationView
 
     //inisialisasi semua fragment dari driver
@@ -33,146 +34,62 @@ class DriverMainActivity : AppCompatActivity() {
 
     //untuk user yang sedang login
     lateinit var userActive : User
-    var unameActive = ""
-
-    //web service :
-    val WS_HOST = "http://10.0.2.2:8000/api"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driver_main)
 
+        // Initialize UI components
         navbar_driver = findViewById(R.id.navbar_driver)
 
-        userActive = User(-1,"","","","","","",-1,"")
+        // Initialize Variables
+        userActive = intent.getSerializableExtra("active_user") as User // Get the logged user
 
-        //ambil data dari intent
-        val usernameActive = intent.getStringExtra("active_username")
-        if(usernameActive!=null){
-            unameActive = usernameActive
-            getUserLoggedIn(usernameActive)
-        }
+        // Load the fragments for this activity
+        loadFragments()
 
-        //load semua fragment yang ada
-        loadFragmentHome()
-        loadFragmentProfile()
-        loadFragmentListAvailablePackages()
-        loadFragmentListCanceledPackages()
-        loadFragmentListDeliverPackages()
-        loadFragmentDetailAvailablePackage()
-        loadFragmentDetailDeliverPackage()
-        loadFragmentDetailCanceledPackage()
-        loadFragmentToFinishPackage()
+        switchFragment(R.id.fragment_container_driver, fragmentHome)
 
-        switchFragment(R.id.fragment_container_driver,fragmentHome)
-
+        // Add navigation event
         navbar_driver.setOnItemSelectedListener {
-
-            if(it.itemId == R.id.item_home_driver){
-                switchFragment(R.id.fragment_container_driver,fragmentHome)
-            }else if(it.itemId == R.id.item_packages_driver){
-                switchFragment(R.id.fragment_container_driver,fragmentListAvailablePackages)
-            }else if(it.itemId == R.id.item_delivered_driver){
-                switchFragment(R.id.fragment_container_driver,fragmentListDeliverPackages)
-            }else if(it.itemId == R.id.item_canceled_driver){
-                switchFragment(R.id.fragment_container_driver,fragmentListCanceledPackages)
-            }else if(it.itemId == R.id.item_profile_driver){
-                switchFragment(R.id.fragment_container_driver,fragmentProfile)
+            // Switches the fragment based on what button is clicked
+            when (it.itemId) {
+                R.id.item_home_driver ->
+                    switchFragment(R.id.fragment_container_driver, fragmentHome)
+                R.id.item_packages_driver ->
+                    switchFragment(R.id.fragment_container_driver, fragmentListAvailablePackages)
+                R.id.item_delivered_driver ->
+                    switchFragment(R.id.fragment_container_driver, fragmentListDeliverPackages)
+                R.id.item_canceled_driver ->
+                    switchFragment(R.id.fragment_container_driver, fragmentListCanceledPackages)
+                R.id.item_profile_driver ->
+                    switchFragment(R.id.fragment_container_driver, fragmentProfile)
             }
-
             return@setOnItemSelectedListener true
         }
-
     }
 
-    //to get user who logged in
-    fun getUserLoggedIn(uname:String){
-        val strReq = object: StringRequest(
-            Method.GET,
-            "$WS_HOST/listUsers",
-            Response.Listener {
-                val obj: JSONArray = JSONArray(it)
-                println(obj.length())
-                for (i in 0 until obj.length()){
-                    val o = obj.getJSONObject(i)
-                    println(o)
-                    val id = o.getInt("id")
-                    val username = o.getString("username")
-                    val password = o.getString("password")
-                    val full_name = o.getString("full_name")
-                    val phone = o.getString("phone")
-                    val address = o.getString("address")
-                    val email = o.getString("email")
-                    val role = o.getInt("role")
-                    val deleted_at = o.get("deleted_at").toString()
-                    val u = User(
-                        id,username,password,full_name,phone,address,email,role,deleted_at
-                    )
-                    if(username==uname) {
-                        userActive = u
-                        break
-                    }
-                }
-                println(userActive)
-            },
-            Response.ErrorListener {
-                Toast.makeText(this,"ERROR!", Toast.LENGTH_SHORT).show()
-            }
-        ){}
-        val queue: RequestQueue = Volley.newRequestQueue(this)
-        queue.add(strReq)
-    }
-
-    //load semua fragments :
-    //load fragment home :
-    fun loadFragmentHome(){
+    /**
+     * Loads the fragments needed for this activity.
+     */
+    fun loadFragments() {
         fragmentHome = DriverHomeFragment()
-    }
-
-    //load fragment list packages yang available :
-    fun loadFragmentListAvailablePackages(){
         fragmentListAvailablePackages = DriverPackagesFragment()
-    }
-
-    //load fragment list packages yang sedang diantar :
-    fun loadFragmentListDeliverPackages(){
         fragmentListDeliverPackages = DriverListDeliverFragment()
-    }
-
-    //load fragment list packages yang telah di cancel :
-    fun loadFragmentListCanceledPackages(){
         fragmentListCanceledPackages = DriverListCanceledFragment()
-    }
-
-    //load fragment ke halaman profile :
-    fun loadFragmentProfile(){
-        fragmentProfile = UserProfileFragment(unameActive)
-    }
-
-    //load detail page dari paket yang sedang tersedia yang dipilih :
-    fun loadFragmentDetailAvailablePackage(){
+        fragmentProfile = UserProfileFragment(userActive.username)
         fragmentDetailAvailablePackage = DriverPackageDetailFragment()
-    }
-
-    //load detail page dari paket yang sedang diantarkan :
-    fun loadFragmentDetailDeliverPackage(){
         fragmentDetailDeliverPackage = DriverDeliveredPackageDetailFragment()
-    }
-
-    //load detail page dari paket yang di cancel :
-    fun loadFragmentDetailCanceledPackage(){
         fragmentDetailCanceledPackage = DriverCanceledPageDetailFragment()
-    }
-
-    //load fragment untuk menyelesaikan sebuah kiriman
-    fun loadFragmentToFinishPackage(){
         fragmentFinishPackage = DriverFinishDetailFragment()
     }
 
     //fungsi untuk berganti fragment
-    fun switchFragment(containerViewId:Int,fragment: Fragment){
+    fun switchFragment(containerViewId:Int, fragment: Fragment){
         val bundle = Bundle()
+        bundle.putSerializable("active_user", userActive)
         fragment.arguments = bundle
+
         val fragmentManager = supportFragmentManager.beginTransaction()
         fragmentManager.replace(containerViewId, fragment)
         fragmentManager.commit()
@@ -184,11 +101,9 @@ class DriverMainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         if(item.itemId == R.id.item_logout){
             finish()
         }
-
         return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/clarissa/thewholeshare/fragments/DriverHomeFragment.kt
+++ app/src/main/java/com/clarissa/thewholeshare/fragments/DriverHomeFragment.kt
@@ -1,18 +1,36 @@
 package com.clarissa.thewholeshare.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
 import com.clarissa.thewholeshare.R
+import com.clarissa.thewholeshare.api.WholeShareApiService
+import com.clarissa.thewholeshare.models.User
 
 
 class DriverHomeFragment : Fragment() {
+    // Components
+    lateinit var availablePacketsLabel: TextView
+    lateinit var ongoingPacketsLabel: TextView
+    lateinit var canceledPacketsLabel: TextView
+    lateinit var finishedPacketsLabel: TextView
+
+    // Variable
+    lateinit var activeUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Initialize Variables
+        activeUser = this.requireArguments().get("active_user") as User
+//        Log.d("ACTIVE_USER", "onCreate: ${activeUser}")
     }
 
     override fun onCreateView(
@@ -23,5 +41,113 @@ class DriverHomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_driver_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // Initialize components
+        availablePacketsLabel = view.findViewById(R.id.tv_totalPackAvailable)
+        ongoingPacketsLabel = view.findViewById(R.id.tv_totalPackOnGoing)
+        canceledPacketsLabel = view.findViewById(R.id.tvTotalPackCanceled_HomeAdmin)
+        finishedPacketsLabel = view.findViewById(R.id.tvTotalFinished_homeAdmin)
+
+        // Load Events
+        countAllAvailablePackets(availablePacketsLabel)
+        countAllOngoingPackets(ongoingPacketsLabel)
+        countAllCancelledPackets(canceledPacketsLabel)
+        countAllFinishedPackets(finishedPacketsLabel)
+
+        // Register Events
+    }
+
+    /**
+     * Fetch the total count of packets that is currently available and have not yet been taken by other courier.
+     * Display the successful count message to the available counter in a card.
+     *
+     * @param label A TextView that will output the count result of the response.
+     */
+    private fun countAllAvailablePackets(label: TextView) {
+        // Make the request object
+        val availableRequest = StringRequest(Request.Method.GET, "${WholeShareApiService.WS_HOST}/requests/countAvailable",
+            {
+                label.text = it
+            },
+            {
+                Log.d("AVAILABLE_PACKETS", "ERROR: ${it.message}")
+            }
+        )
+        // Set the request timeout to 5 second before attempting to retrying
+        availableRequest.setRetryPolicy(DefaultRetryPolicy(5000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT))
+
+        // Add the request object to the queue to process
+        WholeShareApiService.getInstance(this.requireContext()).addToRequestQueue(availableRequest)
+    }
+
+    /**
+     * Fetch the total count of packets that is currently ongoing and have been taken by you.
+     * Display the successful count message to the ongoing counter in a card.
+     *
+     * @param label A TextView that will output the count result of the response.
+     */
+    private fun countAllOngoingPackets(label: TextView) {
+        // Make the request object
+        val ongoingRequest = StringRequest(Request.Method.GET, "${WholeShareApiService.WS_HOST}/requests/countOngoing?user_id=${activeUser.id}",
+            {
+                label.text = it
+            },
+            {
+                Log.d("ONGOING_PACKETS", "ERROR: ${it.message}")
+            }
+        )
+        // Set the request timeout to 5 second before attempting to retrying
+        ongoingRequest.retryPolicy = DefaultRetryPolicy(5000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
+
+        // Add the request object to the queue to process
+        WholeShareApiService.getInstance(this.requireContext()).addToRequestQueue(ongoingRequest)
+    }
+
+    /**
+     * Fetch the total count of packets that is cancelled.
+     * Display the successful count message to the ongoing counter in a card.
+     *
+     * @param label A TextView that will output the count result of the response.
+     */
+    private fun countAllCancelledPackets(label: TextView) {
+        // Make the request object
+        val cancelledRequest = StringRequest(Request.Method.GET, "${WholeShareApiService.WS_HOST}/requests/countCancelled?user_id=${activeUser.id}",
+            {
+                label.text = it
+            },
+            {
+                Log.d("CANCELLED_PACKETS", "ERROR: ${it.message}")
+            }
+        )
+        // Set the request timeout to 5 second before attempting to retrying
+        cancelledRequest.retryPolicy = DefaultRetryPolicy(5000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
+
+        // Add the request object to the queue to process
+        WholeShareApiService.getInstance(this.requireContext()).addToRequestQueue(cancelledRequest)
+    }
+
+    /**
+     * Fetch the total count of packets that is finished by you.
+     * Display the successful count message to the ongoing counter in a card.
+     *
+     * @param label A TextView that will output the count result of the response.
+     */
+    private fun countAllFinishedPackets(label: TextView) {
+        // Make the request object
+        val finishedRequest = StringRequest(Request.Method.GET, "${WholeShareApiService.WS_HOST}/requests/countFinished?user_id=${activeUser.id}",
+            {
+                label.text = it
+            },
+            {
+                Log.d("FINISHED_PACKETS", "ERROR: ${it.message}")
+            }
+        )
+        // Set the request timeout to 5 second before attempting to retrying
+        finishedRequest.retryPolicy = DefaultRetryPolicy(5000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
+
+        // Add the request object to the queue to process
+        WholeShareApiService.getInstance(this.requireContext()).addToRequestQueue(finishedRequest)
+    }
 }
