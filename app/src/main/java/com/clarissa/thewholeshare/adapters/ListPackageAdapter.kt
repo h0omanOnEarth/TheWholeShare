7eb6a68f35b446dd0c72ac20fef3cd7f74498b87
sachio271 <105487625+sachio271@users.noreply.github.com>
2023-01-12 08:13:49 +0700

    update list package

---
+++ app/src/main/java/com/clarissa/thewholeshare/adapters/ListPackageAdapter.kt
@@ -0,0 +1,39 @@
+package com.clarissa.thewholeshare.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.clarissa.thewholeshare.R
+import com.clarissa.thewholeshare.models.Location
+import com.clarissa.thewholeshare.models.Participant
+
+class ListPackageAdapter(
+    var context: Context,
+    var data: ArrayList<Participant>
+):RecyclerView.Adapter<ListPackageAdapter.MyHolder>() {
+    class MyHolder (it: View) : RecyclerView.ViewHolder(it){
+        var tvid: TextView = it.findViewById(R.id.tvidpackage)
+        var tvloc: TextView = it.findViewById(R.id.tvjalan)
+        var tvnama: TextView = it.findViewById(R.id.tvfullname)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        var convertview = LayoutInflater.from(context).inflate(R.layout.item_package, parent, false)
+        return ListPackageAdapter.MyHolder(convertview)
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        val item = data[position]
+        holder.tvid.setText("#" + item.id)
+        holder.tvloc.setText(item.pickup)
+        holder.tvnama.setText(item.fullname)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
